7ffecc9176a281ba8d8b4a19015dafa974b546bc
Domenico Rizzo <dev@idsign.it>
2018-01-19 10:04:22 +0100

    Added super user in controllers

--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -4,6 +4,11 @@ namespace Idsign\Permission\Http\Controllers;
 
 class RoleController extends PermissionRoleSectionController
 {
+    public function __construct()
+    {
+        $this->addPermittedRoles([config('permission.roles.superuser')]);
+    }
+
     protected function delta() : string
     {
         return self::ROLE;
